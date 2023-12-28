24e84a8999904151e701c8e2050e16716efd15aa
Suraj313 <144615470+Suraj313@users.noreply.github.com>
2023-12-28 20:12:42 +0530

    SecondLargestElementinArray.cpp

---
+++ ARRAY/SecondLargestElementinArray.cpp
@@ -0,0 +1,21 @@
+int secondLargest(int n, vector<int> &arr)
+{
+	if(n<2)
+	return -1;
+    int large=INT_MIN,second_large=INT_MIN;
+    int i;
+    for (i = 0; i < n; i++) 
+    {
+        if (arr[i] > large) 
+        {
+            second_large = large;
+            large = arr[i];
+        }
+ 
+        else if (arr[i] > second_large && arr[i] != large) 
+        {
+            second_large = arr[i];
+        }
+    }
+    return second_large;                
+}
